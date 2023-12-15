684de5676a56b36643e50308f55c03594aafaff8
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2023-12-15 16:46:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,4 +7,10 @@ function generateQR(){
         qr_img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
         qr_img.classList.add("show-box");
     }
+    else{
+        qrText.classList.add("error");
+        setTimeout(()=>{
+            qrText.classList.remove("error");
+        },1000);
+    }
 }
