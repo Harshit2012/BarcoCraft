315485da326aedf51afaef92f1f6c20d1aea1b0c
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2023-12-23 10:59:58 +0530

    Update main.js
    Updating main.js to show popup

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let qr_box = document.getElementById("qr-box");
 let qr_img = document.getElementById("qr-img");
 let qrText = document.getElementById("qrText");
+let popup = document.getElementById("popup");
+
+function open_popup(){
+    popup.classList.add("open_popup");
+}
 
 function generateQR(){
     if(qrText.value.length > 0){
         qr_img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
         qr_img.classList.add("show-box");
+        open_popup();
     }
     else{
         qrText.classList.add("error");
@@ -13,4 +19,8 @@ function generateQR(){
             qrText.classList.remove("error");
         },1000);
     }
+}
+
+function closePopup(){
+    popup.classList.remove("open_popup");
 }
